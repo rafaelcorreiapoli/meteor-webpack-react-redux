71c7806b47e4d76d51fafe36b04cc16a13858da4
rafaelcorreiapoli <rafael.correia.poli@gmail.com>
2016-08-31 21:48:59 -0300

    Counter example

--- src/client/pages/Welcome/index.js
+++ src/client/pages/Welcome/index.js
@@ -1,10 +1,11 @@
 import React, { PropTypes } from 'react'
+import Counter from 'client/containers/Counter'
 
 class Welcome extends React.Component {
   render() {
     return (
       <div>
-        Welcome
+        <Counter />
       </div>
     )
   }
--- src/client/reducers/index.js
+++ src/client/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 
 import { reducer as formReducer } from 'redux-form';
 import { routerReducer } from 'react-router-redux'
+import counter from 'client/ducks/counter'
 
 const rootReducer = combineReducers({
   form: formReducer,
   routing: routerReducer,
+  counter,
 });
 
 export default rootReducer
